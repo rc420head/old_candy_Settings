c51af5ee10614047bb039227bae6044e277aefbb
matthew0776 <matthew0776@hotmail.com>
2014-12-20 01:18:45 -0600

    Bring battery text options inline with Cm

--- src/com/android/settings/candy/StatusBar.java
+++ src/com/android/settings/candy/StatusBar.java
@@ -34,26 +34,41 @@ import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
-public class StatusBar extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+public class StatusBar extends SettingsPreferenceFragment 
+      implements OnPreferenceChangeListener {
 
     private static final String TAG = "StatusBarSettings";
+ private static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
 
+ private ListPreference mStatusBarBattery;
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
 
         addPreferencesFromResource(R.xml.status_bar_settings);
+     ContentResolver resolver = getActivity().getContentResolver();
 
-    }
+     mStatusBarBattery = (ListPreference) findPreference(STATUS_BAR_SHOW_BATTERY_PERCENT);
 
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        return false;
+   int batteryStyle = Settings.System.getInt(
+      resolver, Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, 0);
+       mStatusBarBattery.setValue(String.valueOf(batteryStyle));
+       mStatusBarBattery.setSummary(mStatusBarBattery.getEntry());
+       mStatusBarBattery.setOnPreferenceChangeListener(this);
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
+   public boolean onPreferenceChange(Preference preference, Object newValue) {
+      ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mStatusBarBattery) {
+   int batteryStyle = Integer.valueOf((String) newValue);
+      int index = mStatusBarBattery.findIndexOfValue((String) newValue);
+         Settings.System.putInt(
+        resolver, Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, batteryStyle);
+    mStatusBarBattery.setSummary(mStatusBarBattery.getEntries()[index]);
+          return true;
+         }
+        return false;
     }
-
 }
