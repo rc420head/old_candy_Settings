2340d4dda1c3ae2478fdd1d4e6ac1be656bd4d03
matthew0776 <matthew0776@hotmail.com>
2014-12-21 19:28:43 -0600

    Add:Quick Unlock 1/1
    thanks Slim @kufikugel

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -85,6 +85,7 @@ public class SecuritySettings extends SettingsPreferenceFragment
     private static final String KEY_OWNER_INFO_SETTINGS = "owner_info_settings";
     private static final String KEY_ADVANCED_SECURITY = "advanced_security";
     private static final String KEY_MANAGE_TRUST_AGENTS = "manage_trust_agents";
+    private static final String LOCKSCREEN_QUICK_UNLOCK_CONTROL = "quick_unlock_control";
 
     private static final int SET_OR_CHANGE_LOCK_METHOD_REQUEST = 123;
     private static final int CONFIRM_EXISTING_FOR_BIOMETRIC_WEAK_IMPROVE_REQUEST = 124;
@@ -134,6 +135,7 @@ public class SecuritySettings extends SettingsPreferenceFragment
     private SwitchPreference mToggleAppInstallation;
     private DialogInterface mWarnInstallApps;
     private SwitchPreference mPowerButtonInstantlyLocks;
+    private SwitchPreference mQuickUnlockScreen;
 
     private ListPreference mSmsSecurityCheck;
 
@@ -310,6 +312,14 @@ public class SecuritySettings extends SettingsPreferenceFragment
                 securityCategory.removePreference(root.findPreference(KEY_VISIBLE_GESTURE));
             }
         }
+
+         // Quick Unlock Screen Control
+         mQuickUnlockScreen = (SwitchPreference) root
+              .findPreference(LOCKSCREEN_QUICK_UNLOCK_CONTROL);
+           if (mQuickUnlockScreen != null) {
+              mQuickUnlockScreen.setChecked(Settings.Secure.getInt(getContentResolver(),
+             Settings.Secure.LOCKSCREEN_QUICK_UNLOCK_CONTROL, 1) == 1);
+       }
 
         // Append the rest of the settings
         addPreferencesFromResource(R.xml.security_settings_misc);
@@ -739,6 +749,11 @@ public class SecuritySettings extends SettingsPreferenceFragment
             Settings.Secure.putInt(getContentResolver(), Settings.Global.SMS_OUTGOING_CHECK_MAX_COUNT,
                     smsSecurityCheck);
             updateSmsSecuritySummary(smsSecurityCheck);
+            
+       } else if (preference == mQuickUnlockScreen) {
+            Settings.Secure.putInt(getActivity().getApplicationContext().getContentResolver(),
+            Settings.Secure.LOCKSCREEN_QUICK_UNLOCK_CONTROL,
+             (Boolean) value ? 1 : 0);
         }
         return result;
     }
