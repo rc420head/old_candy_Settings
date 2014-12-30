356d9343f320171c357a1a46615f0d3c3c84de70
deadman96385 <seanhoyt963@gmail.com>
2014-12-30 02:48:00 -0500

    Return: Listanimation views and interpolator [2/2]
    Change-Id: I0187c4a1c855fb4f7b4f7698fb99d8d3473b2170
    Signed-off-by: errantzebra845 <errantzebra845@gmail.com>
    
    Conflicts:
    	res/values/liquid_arrays.xml->candy_arrays.xml
    	res/xml/liquid_interface_settings.xml->candy_main_settings.xml
    	src/com/android/settings/liquid/InterfaceSettings.java->/candy/MainSettings.xml
    
     Author:    deadman96385 <seanhoyt963@gmail.com>
    
    Conflicts:
    	res/xml/candy_main_settings.xml
    	src/com/android/settings/candy/MainSettings.java

--- src/com/android/settings/candy/AnimationsSettings.java
+++ src/com/android/settings/candy/AnimationsSettings.java
@@ -50,9 +50,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class MainSettings extends SettingsPreferenceFragment  implements
+public class AnimationsSettings extends SettingsPreferenceFragment  implements
         Preference.OnPreferenceChangeListener, Indexable {
-private static final String TAG = "MainSettings";
+private static final String TAG = "AnimationsSettings";
+
+
+    private static final String KEY_LISTVIEW_ANIMATION = "listview_animation";
+    private static final String KEY_LISTVIEW_INTERPOLATOR = "listview_interpolator";
+
+    private ListPreference mListViewAnimation;
+    private ListPreference mListViewInterpolator;
 
          @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -60,16 +67,50 @@ private static final String TAG = "MainSettings";
          final Activity activity = getActivity();
          final ContentResolver resolver = activity.getContentResolver();
 
-        addPreferencesFromResource(R.xml.candy_main_settings);
+        addPreferencesFromResource(R.xml.animations_settings);
           
         PreferenceScreen prefSet = getPreferenceScreen();
 
-      }
-     
+
+
+        // List view animation
+        mListViewAnimation = (ListPreference) findPreference(KEY_LISTVIEW_ANIMATION);
+        int listviewanimation = Settings.System.getInt(getContentResolver(),
+                Settings.System.LISTVIEW_ANIMATION, 0);
+        mListViewAnimation.setValue(String.valueOf(listviewanimation));
+        mListViewAnimation.setSummary(mListViewAnimation.getEntry());
+        mListViewAnimation.setOnPreferenceChangeListener(this);
+
+        mListViewInterpolator = (ListPreference) findPreference(KEY_LISTVIEW_INTERPOLATOR);
+        int listviewinterpolator = Settings.System.getInt(getContentResolver(),
+                Settings.System.LISTVIEW_INTERPOLATOR, 0);
+        mListViewInterpolator.setValue(String.valueOf(listviewinterpolator));
+        mListViewInterpolator.setSummary(mListViewInterpolator.getEntry());
+        mListViewInterpolator.setOnPreferenceChangeListener(this);
+        mListViewInterpolator.setEnabled(listviewanimation > 0);
+        }
+        
          @Override
      public boolean onPreferenceChange(Preference preference, Object objValue) {
         final String key = preference.getKey();
-        
+        if (KEY_LISTVIEW_ANIMATION.equals(key)) {
+            int value = Integer.parseInt((String) objValue);
+            int index = mListViewAnimation.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.LISTVIEW_ANIMATION,
+                    value);
+            mListViewAnimation.setSummary(mListViewAnimation.getEntries()[index]);
+            mListViewInterpolator.setEnabled(value > 0);
+        }
+        if (KEY_LISTVIEW_INTERPOLATOR.equals(key)) {
+            int value = Integer.parseInt((String) objValue);
+            int index = mListViewInterpolator.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.LISTVIEW_INTERPOLATOR,
+                    value);
+            mListViewInterpolator.setSummary(mListViewInterpolator.getEntries()[index]);
+        }
+      
         return true;
       }
       
